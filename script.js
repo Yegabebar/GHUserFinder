e0d5a16c3177be91e2641ff4bbb269bcf36ae055
Yegabebar <m.barbegayet@gmail.com>
2021-01-12 23:04:08 +0100

    Remplacé base de requête API, adaptation en cours

--- script.js
+++ script.js
@@ -1,53 +1,47 @@
-function getUser(){
+async function getUser(){
     //Create request
-    let request = new XMLHttpRequest(); //Create object
-    request.open('GET',url); //First parameter GET/POST, 2nd param: url
-    request.responseType = 'json'; //Type of return we want
-    request.send(); //Sending the request
 
-    //This function will be triggered as soon as we'll get a reply
-    request.onload = function() {
-        if(request.readyState === XMLHttpRequest.DONE){ //If the request has been done
-            if(request.status === 200){ //If the request status is correct,
-                let response = request.response; //Stores the reply
-                //let data = JSON.parse(response);
+    const reponse = await fetch(url);
+    const data = await reponse.json();
+    const {avatar_url} = data;
+    const {name} = data;
+    const {followers} = data;
+    const {public_repos} = data;
+    const {following} = data;
+    //console.log(avatar_url);
 
-                //We create the elements
-                var div = document.createElement("div");
-                var followersP = document.createElement("p");
-                var reposP = document.createElement("p");
-                var followingP = document.createElement("p");
+    //We create the elements 
+    var div = document.createElement("div");
+    var followersP = document.createElement("p");
+    var reposP = document.createElement("p");
+    var followingP = document.createElement("p");
+    
+    //Assign a text to it
+    followersP.textContent = followers;
+    
+    reposP.textContent = repos;
+    followingP.textContent = following;
 
+    
+    console.log("Valeur stockée "+followersP);
+    //followers, following, name, public_repos, avatar_url
+    
+    //Add these elements to the new div
+    div.innerHTML.value+=followersP+'<br>';
+    div.innerHTML.value+=reposP+'<br>';
+    div.innerHTML.value+=followingP;
+    
+    console.log("Valeur dans div avant ajout dans HTML: "+div);
 
-                console.log("Valeur brute "+response.followers); 
+    //Then add the div to the HTML page
+    container.innerHTML=div.followersP.innerHTML;
+    container.innerHTML=div.reposP.innerHTML;
+    container.innerHTML=div.followingP;
+    //Then display the informations to the DOM in a newly-created div
+    console.log('test: '+div.followersP);
 
-                //Assign a text to it
-                followersP.textContent = response.followers;
-                reposP.textContent = response.public_repos;
-                followingP.textContent = response.following;
-
-                
-                console.log("Valeur stockée "+followersP.innerHTML);
-                //followers, following, name, public_repos, avatar_url
-                
-                //Add these elements to the new div
-                div.innerHTML+=followersP+'<br>';
-                div.innerHTML+=reposP+'<br>';
-                div.innerHTML+=followingP;
-                
-                console.log("Valeur dans div avant ajout dans HTML: "+div.innerHTML);
-
-                //Then add the div to the HTML page
-                container.innerHTML=div.followersP.innerHTML;
-                container.innerHTML=div.reposP.innerHTML;
-                container.innerHTML=div.followingP.innerHTML;
-                //Then display the informations to the DOM in a newly-created div
-                console.log('test: '+div.followersP);
-            }else{
-                alert('An error occurred, please try again later');
-            }
-        }
-    }
+        
+    
 }
 
 var username = "";
